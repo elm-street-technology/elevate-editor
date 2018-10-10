fd30bc130b65cc1f01993a5e855ff2c47690f77b
Chris Heninger <chris.heninger@knledg.com>
2018-10-10 10:01:23 -0700

    Convert demo to class and add button

--- demo/src/EmailEditor.js
+++ demo/src/EmailEditor.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import classNames from "classnames";
 import withStyles from "elevate-ui/withStyles";
 import Paper from "elevate-ui/Paper";
+import Button from "elevate-ui/Button";
 import Editor from "elevate-editor";
 
 type Props = {
@@ -9,11 +10,25 @@ type Props = {
   className: string,
 };
 
-const EmailEditor = ({ classes, className }: Props) => (
-  <Paper className={classNames(classes.root, className)}>
-    <Editor components={[]} />
-  </Paper>
-);
+class EmailEditor extends Component<Props> {
+  onClick = () => {
+    console.log(this.editor);
+  };
+  render() {
+    const { classes, className } = this.props;
+    return (
+      <Paper className={classNames(classes.root, className)}>
+        <Editor
+          components={[]}
+          innerRef={(editor) => {
+            this.editor = editor;
+          }}
+        />
+        <Button onClick={this.onClick}>Click me</Button>
+      </Paper>
+    );
+  }
+}
 
 const styles = (theme) => ({
   root: {
