656d13869889cb9a026b89d53af2321db2b529c4
Paul Harvey <pharvpro@gmail.com>
2019-12-05 15:58:10 -0600

    Fix bug in flattenObject function due to JavaScript treating null as type "object"

--- src/utils/flatten-object.js
+++ src/utils/flatten-object.js
@@ -1,9 +1,11 @@
 export default function flattenObject(obj, separator = ".", prefix = "") {
   return Object.keys(obj).reduce((acc, k) => {
     const pre = prefix.length ? prefix + separator : "";
-    if (typeof obj[k] === "object")
+    if (typeof obj[k] === "object" && obj[k] !== null) {
       Object.assign(acc, flattenObject(obj[k], separator, pre + k));
-    else acc[pre + k] = obj[k];
+    } else {
+      acc[pre + k] = obj[k];
+    }
     return acc;
   }, {});
 }
